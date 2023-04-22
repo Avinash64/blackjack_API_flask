46aadba5967d6514428ff7792c133e5b4488b099
hop <avinashseelall@gmail.com>
2023-04-22 19:11:41 -0400

    game_logic

--- blackjack.py
+++ blackjack.py
@@ -11,6 +11,22 @@ def new_deck():
             deck.append(face+'_'+suit)
     return deck
 
+def value(hand):
+    total = 0
+    for i in hand:
+        card = i.split("_")[0]
+        print(card)
+        if card in "JQK":
+            total+= 10
+        elif card == "A":
+            if total >= 11: total+= 1
+            else: 
+                total+= 11
+        else:
+            total += int(card)
+    return total
+    
+
 def new_game():
     return {
         "deck" : new_deck(),
@@ -19,6 +35,18 @@ def new_game():
         "dealerHand": []
     }
 
+
+def deal(game_id, hand):
+    games[game_id][hand].append(games[game_id]["deck"].pop(0))
+
+def start_game(game_id):
+    if game_id in games.keys():
+        for i in range(2):
+            # games[game_id]["playerHand"].append(games[game_id]["deck"].pop(0))
+            # games[game_id]["dealerHand"].append(games[game_id]["deck"].pop(0))
+            deal(game_id, "playerHand")
+            deal(game_id, "dealerHand")
+
 def create_new_game():
     game_id = random.randint(0,10000)
     while game_id in games.keys():
@@ -26,10 +54,18 @@ def create_new_game():
     games[game_id] = new_game()
     return game_id
 
+def update(game_id, action):
 
+    #0 - Start Game
+    if action == 0:
+        if len(games[game_id]["playerHand"]) == 0:
+            start_game(game_id)
 
-def start_game(game_id):
-    if game_id in games.keys():
-        for i in range(2):
-            games[game_id]["playerHand"].append(games[game_id]["deck"].pop(0))
-            games[game_id]["dealerHand"].append(games[game_id]["deck"].pop(0))
+    #1 - Hit
+    if action == 1:
+        deal(game_id, "playerHand")
+
+    #2 - Stand
+    if action == 2:
+        while value(games[game_id]["dealerHand"]) < 17:
+            deal(game_id, "dealerHand")
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from markupsafe import escape
-from flask import Flask
-
+from flask import Flask, request
 from blackjack import *
 
 app = Flask(__name__)
@@ -21,3 +20,19 @@ def create_game():
     game_id = create_new_game()
     start_game(game_id)
     return {"gameId" : game_id, "game": games[game_id]}
+
+
+
+# @app.route('/create_game')
+# def create_game():
+#     game_id = create_new_game()
+#     start_game(game_id)
+#     return {"gameId" : game_id, "game": games[game_id]}
+
+@app.route('/play',methods = ['POST'])
+def play():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        update(json["game_id"], json["action"])
+    return games[json["game_id"]]
